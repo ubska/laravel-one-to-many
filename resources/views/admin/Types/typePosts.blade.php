d54824d6927d9b57ccf1d8e804b6244762bbb938
ubska <alessandraubska@gmail.com>
2024-09-25 14:15:54 +0200

    feat: add type in show page

--- resources/views/admin/Types/typePosts.blade.php
+++ resources/views/admin/Types/typePosts.blade.php
@@ -4,12 +4,14 @@
     <h1>Elenco post per categoria</h1>
     @foreach ($types as $type)
         <h2 class="mt-5">{{ $type->name }}</h2>
-        <ul class="list-group">
-            <li class="list-group-item">An item</li>
-            <li class="list-group-item">A second item</li>
-            <li class="list-group-item">A third item</li>
-            <li class="list-group-item">A fourth item</li>
-            <li class="list-group-item">And a fifth one</li>
+        <ul class="list-group ">
+            @foreach ($type->posts as $post)
+                <li class="list-group-item d-flex justify-content-between">
+                    <span>{{ $post->title }}</span>
+                    <a href="{{ route('admin.posts.show', $post) }}" class="btn btn-warning">Show</a>
+                </li>
+            @endforeach
+
         </ul>
     @endforeach
 @endsection
--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     <h1>{{ $post->title }}</h1>
+    <p>Categoria:{{ $post->type->name }}</p>
     <p><strong>Tempo di lettura:</strong> {{ $post->reading_time }} minuti</p>
     <p><strong>Contenuto:</strong></p>
     <div>
